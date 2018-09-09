8324159008ce65208197ac22114be0477bea9071
root <root@node156.9.115.17.156.nip.io>
2018-09-09 22:07:58 +0800

    updated to v4

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <body>
         <?php
             echo $_SERVER['SERVER_ADDR'];
-            echo '<p>Version v3</p>'; 
+            echo '<p>Version v4</p>'; 
         ?>
     </body>
 </html>
