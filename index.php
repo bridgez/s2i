2ccbabc02d24de344149cb9a2e4148d05cb384ba
bridgez <xuepz@cn.ibm.com>
2018-09-11 12:15:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <body>
         <?php
             echo $_SERVER['SERVER_ADDR'];
-            echo '<p>Version v66</p>'; 
+            echo '<p>Version v77</p>'; 
         ?>
     </body>
 </html>
